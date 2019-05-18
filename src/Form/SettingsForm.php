d8071a4fbd654e9c08abf8c5a0431b301c5f4aea
Steven Straker <contact@atrakez.com>
2019-05-18 09:34:14 -0400

    Create admin settings form for module

---
+++ src/Form/SettingsForm.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Steven
+ * Date: 2019-05-18
+ * Time: 8:15 AM
+ */
+
+namespace Drupal\zendesk_webform\Form;
+
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class SettingsForm extends ConfigFormBase
+{
+    protected function getEditableConfigNames()
+    {
+        return [
+            'zendesk_webform.adminsettings'
+        ];
+    }
+
+    public function getFormId()
+    {
+        return 'zendesk_webform_form';
+    }
+
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+
+        $config = $this->config('zendesk_webform.adminsettings');
+
+        $form['subdomain'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('Zendesk Subdomain'),
+            '#description' => $this->t('Add the subdomain for your Zendesk account'),
+            '#default_value' => $config->get('subdomain')
+        ];
+
+        $form['admin_email'] = [
+            '#type' => 'email',
+            '#title' => $this->t('Zendesk Account Email'),
+            '#description' => $this->t('The email address of your Zendesk admin account'),
+            '#default_value' => $config->get('admin_email')
+        ];
+
+        $form['web_token'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('Zendesk API Token'),
+            '#description' => $this->t('The API Token required to connect to Zendesk'),
+            '#default_value' => $config->get('web_token')
+        ];
+
+        return parent::buildForm($form, $form_state);
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        parent::submitForm($form, $form_state);
+
+        $this->config('zendesk_webform.adminsettings')
+            ->set('subdomain',$form_state->getValue('subdomain'))
+            ->set('admin_email',$form_state->getValue('admin_email'))
+            ->set('web_token',$form_state->getValue('web_token'))
+            ->save()
+        ;
+    }
+}
